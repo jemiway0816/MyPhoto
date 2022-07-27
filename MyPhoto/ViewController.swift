8c99c88c3ba393b7412b43a79621e32444a95c42
jemiway0816 <jemiway0816@gmail.com>
2022-07-27 20:22:42 +0800

    Initial Commit

---
+++ MyPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPhoto
+//
+//  Created by jemiway0816 on 2022/6/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
